b2817ee518aab85df45082788c4c75a81a9cd85b
dhanushkiren <dhanushrameshkvpt03@gmail.com>
2023-10-16 11:59:47 +0530

    added login page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React, { useEffect } from 'react';
 import "./App.css";
-import Sidebar from "./components/sidebar/Sidebar";
-import Topbar from "./components/topbar/Topbar";
 import { BrowserRouter as Router } from "react-router-dom";
 import ThemeRoutes from "./routes/index"
 
@@ -10,11 +8,7 @@ function App() {
   return (
     <>
       <Router>
-        <Topbar />
-        <div className='appContainer'>
-          <Sidebar />
           <ThemeRoutes />
-        </div>
       </Router>
     </>
   )
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,7 @@
 import { lazy, useEffect, useState } from "react";
 import { Switch, Route } from "react-router-dom";
 import Loadable from '../components/Loadable'
+import Login from "../pages/ResuableComponents/Login/Login";
 
 const Home = Loadable(lazy(() =>import("../pages/home/Home")));
 const UserList = Loadable(lazy(() =>import("../pages/userList/UserList")));
@@ -9,32 +10,43 @@ const NewUser = Loadable(lazy(() =>import("../pages/newUser/NewUser")));
 const ProductsList = Loadable(lazy(() =>import("../pages/productsList/ProductsList")));
 const Staff = Loadable(lazy(() =>import("../pages/Staff/Staff")));
 const Student = Loadable(lazy(() =>import("../pages/Student/Student")));
+const Sidebar = Loadable(lazy(() =>import("../components/sidebar/Sidebar")));
+const Topbar = Loadable(lazy(() =>import("../components/topbar/Topbar")));
 
 const ThemeRoutes = () => {
   return (
-    <Switch>
-      <Route exact path="/home">
-        <Home />
-      </Route>
-      <Route path="/users">
-        <UserList />
-      </Route>
-      <Route path="/user/:userId">
-        <User />
-      </Route>
-      <Route path="/newUser">
-        <NewUser />
-      </Route>
-      <Route path="/products">
-        <ProductsList />
-      </Route>
-      <Route path="/Staff">
-        <Staff />
-      </Route>
-      <Route path="/Student">
-        <Student />
-      </Route>
-    </Switch>
+    <>
+      <Topbar />
+      <div className="appContainer">
+        <Sidebar />
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/users">
+            <UserList />
+          </Route>
+          <Route path="/user/:userId">
+            <User />
+          </Route>
+          <Route path="/newUser">
+            <NewUser />
+          </Route>
+          <Route path="/products">
+            <ProductsList />
+          </Route>
+          <Route path="/Staff">
+            <Staff />
+          </Route>
+          <Route path="/Student">
+            <Student />
+          </Route>
+          <Route path="/login">
+            <Login />
+          </Route>
+        </Switch>
+      </div>
+    </>
   );
 };
 
