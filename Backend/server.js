e4b4b20f002e97e1720fb8b02d1fc466b1c7e447
dhanushkiren <dhanushrameshkvpt03@gmail.com>
2023-10-19 22:37:20 +0530

    added backend file

---
+++ Backend/server.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const mysql = require('mysql');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(cors());
+const port = 8081;
+
+app.use(bodyParser.json());
+
+const db = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'sample_login',
+});
+
+
+db.connect((err) => {
+  if (err) {
+    console.error('Error connecting to MySQL:', err);
+  } else {
+    console.log('Connected to MySQL');
+  }
+});
+
+app.post('/login', (req, res) => {
+  const { username, password } = req.body;
+  const query = `SELECT * FROM admin WHERE uid = ? AND pass = ?`;
+
+  db.query(query, [username, password], (err, result) => {
+    if (err) {
+      console.error('Error executing query:', err);
+      res.status(500).send('Internal Server Error');
+        console.log('Internal Server Error');
+    } else {
+      if (result.length > 0) {
+        console.log("result: ", result);
+        const role = result[0].role;
+        console.log("role : ",role);
+        res.status(200).json({ message: 'Login Successful', role: role });
+        console.log("login success");
+      } else {
+        res.status(401).send('Invalid Credentials');
+        console.log("Invalid Credentials");
+      }
+    }
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
